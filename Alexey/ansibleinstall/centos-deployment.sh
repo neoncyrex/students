2e91e89bbd425721528eb42b07c8fb2ea5889557
neoncyrex <artem.kropachev@gmail.com>
2017-05-26 07:11:29 -0600

    Update centos-deployment.sh

--- Alexey/ansibleinstall/centos-deployment.sh
+++ Alexey/ansibleinstall/centos-deployment.sh
@@ -2,10 +2,8 @@
 echo script start
 sudo yum install epel-release -y
 echo start ansible installation
-sudo yum install ansible -y
-echo ansible is installed, installing git...
-sudo yum install git -y
-echo git installed, creating final stuff
+sudo yum install ansible -y && echo ansible is installed, installing git...
+sudo yum install git -y && echo git installed, creating final stuff
 cd ~
 mkdir ./git
 cd ./git
