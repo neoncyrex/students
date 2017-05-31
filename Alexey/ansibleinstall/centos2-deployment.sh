9e5a5edf3fb6aee197283986203b5c567791a8c5
Alexey R <alexey.y.romanov@gmail.com>
2017-05-31 00:21:47 -0700

    delete junk

--- Alexey/ansibleinstall/centos2-deployment.sh
+++
@@ -1,6 +0,0 @@
-sudo useradd -m mgmt
-sudo echo Ansible123|passwd --stdin mgmt
-sudo echo "mgmt ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/devops
-sudo sed -i.bak -e "s/asswordAuthentication no/asswordAuthentication yes/" /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
