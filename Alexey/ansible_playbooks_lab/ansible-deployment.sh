216dd91e2445530c002203d7e8b54365ea89c077
Alexey R <alexey.y.romanov@gmail.com>
2017-06-03 23:07:36 -0700

    vars

--- Alexey/ansible_playbooks_lab/ansible-deployment.sh
+++ Alexey/ansible_playbooks_lab/ansible-deployment.sh
@@ -7,7 +7,7 @@ sudo useradd -m ansible
 sudo echo Ansible123|passwd --stdin ansible
 sudo echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
 mkdir /home/ansible/.ssh
-chown ansible:ansible /home/devops/.ssh
+chown ansible:ansible /home/ansible/.ssh
 chmod 0700 /home/ansible/.ssh
 ssh-keygen -t rsa -f /home/ansible/.ssh/id_rsa -N ""
 sudo sed -i.bak -e "s/asswordAuthentication no/asswordAuthentication yes/" /etc/ssh/sshd_config
@@ -16,4 +16,7 @@ mkdir /home/vagrant/files
 cp /vagrant/ansible.cfg /home/vagrant/ansible.cfg
 cp /vagrant/inventory /home/vagrant/inventory
 cp /vagrant/files/index.html /home/vagrant/files/index.html
-cp /vagrant/files/httpd.yml /home/vagrant/files/httpd.yml
+cp /vagrant/files/httpd.conf /home/vagrant/files/httpd.conf
+cp /vagrant/httpd.yml /home/vagrant/httpd.yml
+cp /vagrant/noshutdown.yml /home/vagrant/noshutdown.yml
+cp /vagrant/shutdown.yml /home/vagrant/shutdown.yml
--- Alexey/ansible_playbooks_lab/centos2-deployment.sh
+++ Alexey/ansible_playbooks_lab/centos2-deployment.sh
@@ -2,7 +2,7 @@ sudo useradd -m ansible
 sudo echo Ansible123|passwd --stdin ansible
 sudo echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
 mkdir /home/ansible/.ssh
-chown ansible:ansible /home/devops/.ssh
+chown ansible:ansible /home/ansible/.ssh
 chmod 0700 /home/ansible/.ssh
 ssh-keygen -t rsa -f /home/ansible/.ssh/id_rsa -N ""
 sudo sed -i.bak -e "s/asswordAuthentication no/asswordAuthentication yes/" /etc/ssh/sshd_config
